95b10eb3661f4b1b1f2687b84c188fa82aa77b0c
Cengiz <cengiz_eryigit@hotmail.com>
2024-06-07 14:53:22 +0300

    Search API

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 
 import myUserRoute from "./routes/MyUserRoute";
 import MyRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -27,6 +28,7 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", MyRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
